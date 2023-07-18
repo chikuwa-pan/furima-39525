568f74938364823899c909dd34a3dd1d198aa321
Takagi <zkazo@gmx.com>
2023-07-18 17:26:00 +0900

    お名前フリガナ欄に半角禁止バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,14 @@ class User < ApplicationRecord
 
         validates :nickname, presence: true
         validate :validate_email_format
-        validates :last_name, presence: true
-        validates :first_name, presence: true
-        validates :last_name_kana, presence: true
-        validates :first_name_kana, presence: true
+
+        with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+          validates :last_name, presence: true
+          validates :first_name, presence: true
+          validates :last_name_kana, presence: true
+          validates :first_name_kana, presence: true
+        end
+
         validates :date_of_birth, presence: true
 
   def validate_email_format
