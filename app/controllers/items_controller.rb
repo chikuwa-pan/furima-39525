7d031fc8c453d50ca93c23cafbe1f95d05a58564
Takagi <zkazo@gmx.com>
2023-07-24 20:06:19 +0900

    商品情報保存のアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,32 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  def item_params
+    params.require(:item).permit( :image,
+                                  :title, 
+                                  :description, 
+                                  :category_id, 
+                                  :condition_id,
+                                  :postage_id,
+                                  :prefecture_id,
+                                  :shipping_day_id,
+                                  :price)
   end
 end
