45ac654e8dacb002fc53dcb964603b9c723eb401
Takagi <zkazo@gmx.com>
2023-07-24 20:12:15 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,33 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
+
+  validates :user, 
+            :title, 
+            :description, 
+            :category_id, 
+            :condition_id, 
+            :postage_id, 
+            :prefecture_id, 
+            :shipping_day_id, 
+            presence: true
+
+  validates :price, presence: true,
+                    numericality: { 
+                      only_integer: true, 
+                      greater_than_or_equal_to: 300, 
+                      less_than_or_equal_to: 999999 }
+
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :category, numericality: { other_than: 1 }
+    belongs_to_active_hash :condition, numericality: { other_than: 1 }
+    belongs_to_active_hash :postage, numericality: { other_than: 1 }
+    belongs_to_active_hash :prefecture, numericality: { other_than: 1 }
+    belongs_to_active_hash :shipping_day, numericality: { other_than: 1 }
+
+  def was_attached?
+    self.image.attached?
+  end
 end
