3b174666cf42b5fd6dca7f929c5913eac22e5bee
Takagi <zkazo@gmx.com>
2023-07-25 18:02:00 +0900

    金額計算の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,11 +1,23 @@
 const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("keyup", () => {
+priceInput.addEventListener("input", () => {
   const price = parseInt(priceInput.value); // 入力された金額を整数に変換
-  const taxPrice = document.getElementById("add-tax-price"); // 変数taxPriceにtax-priceをいれる
-  const calculator = Math.floor(price * 0.1); // 入力された金額の10%を計算（小数点以下は切り捨て）
-  taxPrice.innerHTML = calculator;
 
-  const profit = price - calculator;
-  const profitElement = document.getElementById("profit");
-  profitElement.innerHTML = profit;
+  // 価格が有効な数値であれば計算結果を表示する
+  if (!isNaN(price)) {
+    const taxPrice = document.getElementById("add-tax-price");
+    const calculator = Math.floor(price * 0.1);
+    taxPrice.innerHTML = calculator;
+
+    const profit = price - calculator;
+    const profitElement = document.getElementById("profit");
+    profitElement.innerHTML = profit;
+  } else {
+    // 価格が無効な数値（NaN）の場合は空にする
+    const taxPrice = document.getElementById("add-tax-price");
+    taxPrice.innerHTML = "";
+
+    const profitElement = document.getElementById("profit");
+    profitElement.innerHTML = "";
+  }
 });
+
