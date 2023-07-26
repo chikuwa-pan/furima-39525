1f4e77a25b13daa1d4721f305f71aeecc8771d12
Takagi <zkazo@gmx.com>
2023-07-26 14:58:42 +0900

    レビューに対する修正1

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   resources :items ,only: [:index, :new, :create, :show]
-  resources :users ,only: [:new, :create, :destroy, :show]
+  resources :users ,only: [:new, :create, :destroy]
 end
