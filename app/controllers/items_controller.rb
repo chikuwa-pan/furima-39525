c3891eb3781ca2b99465092f8305019cd7c76d01
Takagi <zkazo@gmx.com>
2023-07-26 15:55:50 +0900

    編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,16 @@ before_action :move_to_sign_in, only: [:new]
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+      @item.update(item_params)
+      redirect_to @item
+  end
+
   private
   def item_params
     params.require(:item).permit( :image,
