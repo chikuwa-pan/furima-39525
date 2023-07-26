96a74f03fef6d695d3c71f74ecea8df0bbfc1638
Takagi <zkazo@gmx.com>
2023-07-26 11:03:41 +0900

    showへ詳細表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,10 @@ before_action :move_to_sign_in, only: [:new]
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit( :image,
