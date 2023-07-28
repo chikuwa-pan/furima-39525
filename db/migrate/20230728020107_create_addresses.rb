d8b45b34c0fd05dd5f1c653ac4b74fce0bb2341a
Takagi <zkazo@gmx.com>
2023-07-28 11:18:02 +0900

    保存機能作成
    アクション定義、テーブル作成

---
+++ db/migrate/20230728020107_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :addresses do |t|
+      t.references :purchase,         null: false, foreign_key: true
+      t.string :post_number,       null: false
+      t.string :prefecture_id,     null: false
+      t.string :municipalities,    null: false
+      t.string :address,           null: false
+      t.string :building
+      t.string :phone,             null: false
+      t.timestamps
+    end
+  end
+end
