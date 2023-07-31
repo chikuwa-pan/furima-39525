3ed5f19e499f963354df55e3ef744279aff3bd43
Takagi <zkazo@gmx.com>
2023-07-31 21:37:59 +0900

    リダイレクト設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,33 +1,31 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_purchase, only: [:index, :create]
-  before_action :move_to_index, only: :index
 
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @purchase_address = PurchaseAddress.new(item_id: @item.id)
+
+    if Purchase.exists?(item_id: @item.id)
+      redirect_to root_path
+    end
   end
 
+
   def create
-    @purchase_address = PurchaseAddress.new(purchase_params.merge(item_id: @item.id, user_id: current_user.id))
-    if @purchase_address.valid?
-      payjp_charge
-      @purchase_address.save
-      redirect_to root_path
-    else
-      render :index
-    end
+      @purchase_address = PurchaseAddress.new(purchase_params.merge(item_id: @item.id, user_id: current_user.id))
+      if @purchase_address.valid?
+        payjp_charge
+        @purchase_address.save
+        redirect_to root_path
+      else
+        render :index
+      end
   end
 
   private
     def set_purchase
       @item = Item.find(params[:item_id])
-    end
-
-    def move_to_index
-      if current_user == @item.user
-        redirect_to root_path
-      end
     end
 
     def payjp_charge
