b083dc34f63cc4b5da748361fe580f6eff2e7580
Takagi <zkazo@gmx.com>
2023-08-03 15:16:48 +0900

    修正
    JavaScript非同期になってなかった

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -2,30 +2,31 @@ const pay = () => {
   const publicKey = gon.public_key
   const payjp = Payjp(publicKey) // PAY.JPテスト公開鍵
   const elements = payjp.elements();
-  const numberElement = elements.create('cardNumber');
-  const expiryElement = elements.create('cardExpiry');
-  const cvcElement = elements.create('cardCvc');
-
-  numberElement.mount('#number-form'); //カード番号のID
-  expiryElement.mount('#expiry-form'); //有効期限のID
-  cvcElement.mount('#cvc-form'); //セキュリティコードのID
   const form = document.getElementById('charge-form')
-  form.addEventListener("submit", (e) => {
-    payjp.createToken(numberElement).then(function (response) { //戻り値としてカード情報のトークンを取得するやつ
+
+  const numberElement = elements.create('cardNumber'); // カード番号いれる箱
+    const expiryElement = elements.create('cardExpiry'); // 有効期限いれる箱
+    const cvcElement = elements.create('cardCvc'); // セキュリティコード入れる箱
+    numberElement.mount('#number-form'); //カード番号のID
+    expiryElement.mount('#expiry-form'); //有効期限のID
+    cvcElement.mount('#cvc-form'); //セキュリティコードのID
+
+  form.addEventListener("submit", (e) => { // 送信ボタンでイベント発生
+    payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
       } else {
-        const token = response.id; // PAY.JPのサーバーに送ったカード情報をもとに、PAY.JPが発行したトークンをresponse.idから取得
-          const renderDom = document.getElementById("charge-form"); // 購入ページのFORM取得
-          const tokenObj = `<input value=${token} name='token' type="hidden">`; // フォームに埋め込むトークンを、HTMLの<input>要素として作成。トークンをvalue属性にセットし、name属性には'token'という名前を設定。サーバーサイドでトークンを取得し、PAY.JPとのやり取りができるようになーる。
-          renderDom.insertAdjacentHTML("beforeend", tokenObj); // renderDomで取得したフォーム要素の末尾に、先ほど作成した<input>要素を挿入しています。これにより、トークンがフォームに埋め込まれます。
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-      numberElement.clear(); //カード情報を削除
-      expiryElement.clear(); //カード情報を削除
-      cvcElement.clear(); //カード情報を削除
-      document.getElementById("charge-form").submit(); //サーバーに送信
+      numberElement.clear();
+      expiryElement.clear();
+      cvcElement.clear();
+      document.getElementById("charge-form").submit();
     });
     e.preventDefault();
   });
 };
 
-window.addEventListener("load", pay);
+window.addEventListener("turbo:load", pay);
--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,23 +1,19 @@
-const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("keyup", () => {
-  const price = parseInt(priceInput.value); // 入力された金額を整数に変換
-
-  // 価格が有効な数値であれば計算結果を表示する
-  if (!isNaN(price)) {
-    const taxPrice = document.getElementById("add-tax-price");
-    const calculator = Math.floor(price * 0.1);
-    taxPrice.innerHTML = calculator;
-
-    const profit = price - calculator;
-    const profitElement = document.getElementById("profit");
-    profitElement.innerHTML = profit;
-  } else {
-    // 価格が無効な数値（NaN）の場合は空にする
-    const taxPrice = document.getElementById("add-tax-price");
-    taxPrice.innerHTML = "";
-
-    const profitElement = document.getElementById("profit");
-    profitElement.innerHTML = "";
-  }
-});
-
+function calculateProfit () {
+  const itemPrice = document.getElementById("item-price");
+  itemPrice.addEventListener("keyup", () => {
+      const price = parseInt(itemPrice.value); // 入力した金額を取得
+        const calculator = Math.floor(price * 0.1); // 手数料計算
+        const addTaxPrice = document.getElementById("add-tax-price");
+        const profit = document.getElementById("profit");
+    if (!isNaN(price)) {
+        addTaxPrice.innerHTML = calculator; // 手数料表示
+        const profitElement = price - calculator; // 利益計算
+        profit.innerHTML = profitElement; // 利益表示
+    } else {
+        // 価格が無効な数値（NaN）の場合は空にする
+        addTaxPrice.innerHTML = "";
+        profit.innerHTML = "";
+    }
+  });
+};
+window.addEventListener('turbo:load', calculateProfit);
